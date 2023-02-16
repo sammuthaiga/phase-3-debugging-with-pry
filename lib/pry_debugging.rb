dad08feaf128f99bb1bf6b8ab75ac31aaa0d6794
Samuel <samuel.muthaiga@student.moringaschool.com>
2023-02-16 03:34:27 +0300

    commit tree adaf0a3f43b3190653ce1f34215c889e6cc307f8

--- lib/pry_debugging.rb
+++ lib/pry_debugging.rb
@@ -1,6 +1,15 @@
-require 'pry'
+# require 'pry'
+# def plus_two(num)
+# 	num + 2
+# 	# binding.pry
+# 	num
+# end
+
+# def plus_two(num)
+# 	num + 2
+# 	num
+# end
+
 def plus_two(num)
-	num + 2
-	binding.pry
-	num
-end
+	return num + 2
+  end
